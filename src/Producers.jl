193c53f8d7476f7f51b58a0890fd086a388aab10
stepa <solntsev@gmail.com>
2015-02-19 14:56:56 -0600

    broken shuffle! for now

--- src/Producers.jl
+++ src/Producers.jl
@@ -1,5 +1,14 @@
+# function shuffle!(r::AbstractRNG, a::AbstractVector)
+#     for i = length(a):-1:2
+#         j = rand(r, 1:i)
+#         a[i], a[j] = a[j], a[i]
+#     end
+#     return a
+# end
+
 function getSequential(numTrainingPoints,gradientOracle,restoreGradient)
 	indices = [1:numTrainingPoints;]
+	# r = MersenneTwister(1)
 	while true
 		for i in indices
 			let j=i
@@ -8,6 +17,7 @@ function getSequential(numTrainingPoints,gradientOracle,restoreGradient)
 				produce((gg,ccs))
 			end
 		end
+		# shuffle!(r, indices)
 		shuffle!(indices)
 	end
 end
