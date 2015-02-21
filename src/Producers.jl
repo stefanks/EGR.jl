ca797477243653250bc709c6ec4b6ccb37c02748
stepa <solntsev@gmail.com>
2015-02-21 15:42:21 -0600

    shuffle!

--- src/Producers.jl
+++ src/Producers.jl
@@ -1,23 +1,25 @@
-# function shuffle!(r::AbstractRNG, a::AbstractVector)
-#     for i = length(a):-1:2
-#         j = rand(r, 1:i)
-#         a[i], a[j] = a[j], a[i]
-#     end
-#     return a
-# end
+function shuffle!(r::AbstractRNG, a::AbstractVector)
+	for i = length(a):-1:2
+		j=int(floor(rand(r)*i)+1)
+		a[i], a[j] = a[j], a[i]
+	end
+	return a
+end
 
 function getSequential(numTrainingPoints,gradientOracle,restoreGradient)
 	indices = [1:numTrainingPoints;]
-	# r = MersenneTwister(1)
+	r = MersenneTwister(1)
 	while true
 		for i in indices
 			let j=i
+				# println(i)
 				gg=(x)-> gradientOracle(x,j)
 				ccs=(cs)-> restoreGradient(cs,j)
 				produce((gg,ccs))
 			end
 		end
-		# shuffle!(r, indices)
-		shuffle!(indices)
+		println("Shuffling!")
+		shuffle!(r, indices)
+		# shuffle!(indices)
 	end
 end
