391b2947834f0d5ee5fd065f2eed1265d8a9aca5
Stepa <solntsev@gmail.com>
2015-08-05 06:24:33 -0500

    ucb

--- src/EGR.jl
+++ src/EGR.jl
@@ -1,6 +1,6 @@
 module EGR
 
-export MinusPlusOneVector, OutputOpts, alg, getStats, readBin, createBLOracles, L2regGradient, readData, writeBin, SGsd, Opts, VerifyGradient, VerifyRestoration, findBestStepsizeFactor, createMLOracles, StepData, writeFunction, Outputter, ResultFromOO, Problem, returnIfExists, EGRexp, createSBLOracles, writeFinal, getF, getPCC, getMCC, normalizeFeatures, trainTestRandomSeparate, createClassLabels, getStatsHIGGSSUSY, readDataHIGGSSUSY, getStatsIRIS, readDataIRIS, readDataSparse, StrainTestRandomSeparate,DSSsd,DSSexp, getSequentialFinite, getRandom, SAGinit, getSampleFunctionAt, SAGAinit, SSVRG,SSVRGsd,SSVRG,SSVRGfromEGR,SAG,SAGA,getFfinal,SAGcb,SAGAcb, DSS,getSequential,onlyAddBeta1,onlyUpdateBeta1,uLinBeta1,uQuadBeta1,uExpBeta1,USD,SGksd,SGsqrtsd,getFtrainfinal,getFtrain
+export MinusPlusOneVector, OutputOpts, alg, getStats, readBin, createBLOracles, L2regGradient, readData, writeBin, SGsd, Opts, VerifyGradient, VerifyRestoration, findBestStepsizeFactor, createMLOracles, StepData, writeFunction, Outputter, ResultFromOO, Problem, returnIfExists, EGRexp, createSBLOracles, writeFinal, getF, getPCC, getMCC, normalizeFeatures, trainTestRandomSeparate, createClassLabels, getStatsHIGGSSUSY, readDataHIGGSSUSY, getStatsIRIS, readDataIRIS, readDataSparse, StrainTestRandomSeparate,DSSsd,DSSexp, getSequentialFinite, getRandom, SAGinit, getSampleFunctionAt, SAGAinit, SSVRG,SSVRGsd,SSVRG,SSVRGfromEGR,SAG,SAGA,getFfinal,SAGcb,SAGAcb, DSS,getSequential,onlyAddBeta1,onlyUpdateBeta1,uLinBeta1,uQuadBeta1,uExpBeta1,USD,SGksd,SGsqrtsd,getFtrainfinal,getFtrain,UcbonlyAddBeta1,UcbonlyUpdateBeta1,UcbuLinBeta1,UcbuQuadBeta1,UcbuExpBeta1
 
 include("MinusPlusOneVector.jl")
 include("DataPrep.jl")
@@ -13,6 +13,7 @@ include("LossFunctions/L2Reg.jl")
 include("Alg.jl")
 include("Steps/stepSG.jl")
 include("Steps/stepU.jl")
+include("Steps/stepUcb.jl")
 include("Steps/stepDSS.jl")
 include("Steps/stepSAGinit.jl")
 include("Steps/stepSAGAinit.jl")
---
+++ src/Steps/stepUcb.jl
@@ -0,0 +1,143 @@
+type UcbSD <: StepData
+	
+	A # Current sum of gradients. Start with a zeros sparse array
+	f # Current array of gradient functions. Start with zero.
+	  # Has all gradient function in it, no chunking here
+ 	y # Current array of sparse gradient sums. Initialize empty. 
+	  # One gradient sum per chunk
+	I # Current number of total chunks 
+	
+	s::Function # Number of chunks to resample
+	u::Function # Number of new chunks to sample
+	beta::Function
+	getStep::Function
+	stepString::String
+	numVars::Int64
+	sagFsagaT::Bool
+	c
+	
+	function UcbSD(s::Function, u::Function, beta::Function, numVars::Int64, stepString::String,sagFsagaT::Bool,c)
+		new(spzeros(numVars,1),Function[], SparseMatrixCSC[], 0, s, u, beta, ucbComputation, stepString, numVars, sagFsagaT,c)
+	end
+end
+
+function UcbonlyAddBeta1(u::Function, uString::String, numVars::Int64,sagFsagaT::Bool,c)
+	
+	s = (k,I)-> 0
+	beta = (k)->1
+	
+	stepString  = "ucbAb1."*uString*".cs="*string(chunkSize)*"."*string(sagFsagaT)
+	
+	UcbSD(s, u , beta, numVars, stepString,sagFsagaT,c)
+
+end
+
+function UcbonlyUpdateBeta1(s::Function, sString::String, numVars::Int64, ntp::Int64, chunkSize::Int64,sagFsagaT::Bool,c)
+	
+	u = (k,I)-> int(floor(k==0 ? ntp : 0))
+	beta = (k)->1
+	
+	stepString  = "ucbUb1."*sString*".cs="*string(chunkSize)*"."*string(sagFsagaT)
+	
+	UcbSD(s, u , beta, numVars, stepString,sagFsagaT,c)
+end
+
+function UcbuLinBeta1(c::Float64,numVars::Int64,sagFsagaT::Bool,c)
+	
+	if c<1
+		error("C must be >=1")
+	end
+	s = (k,I)-> int(floor(k==0 ? 0 : c))
+	u = (k,I)-> int(c) 
+	beta = (k)->1
+	
+	stepString  = "ucbLb1"*".c="*string(c)*".cs="*string(chunkSize)*"."*string(sagFsagaT)
+	
+	UcbSD(s, u , beta, numVars, stepString,sagFsagaT,c)
+end
+
+function UcbuQuadBeta1(c::Float64,numVars::Int64,sagFsagaT::Bool,c)
+	
+	s = (k,I)-> int(floor(k==0 ? 0 : c*k))
+	u = (k,I)-> int(ceil(c*(k+1)))
+	beta = (k)->1
+	
+	stepString  = "ucbQb1"*".c="*string(c)*".cs="*string(chunkSize)*"."*string(sagFsagaT)
+	
+	UcbSD(s, u , beta, numVars, stepString,sagFsagaT,c)
+end
+
+
+function UcbuExpBeta1(c::Float64, r::Float64, numVars::Int64, chunkSize::Int64,sagFsagaT::Bool,c)
+	
+	s = (k,I)-> int(floor(k==0 ? 0 : c*(r/(r-1))^(k-1)))
+	u = (k,I)-> int(floor(k==0 ? c*(r-1) : c*(r/(r-1))^(k-1))) 
+	beta = (k)->1
+	
+	stepString  = "ucbEb1"*".c="*string(c)*".r="*string(r)*".cs="*string(chunkSize)*"."*string(sagFsagaT)
+	
+	UcbSD(s, u , beta, numVars, stepString,sagFsagaT,c)
+end
+
+
+
+function ucbComputation(x, k, gnum, sd::UcbSD, problem::Problem; outputLevel = 0)
+
+	outputLevel > 0 && println("Starting ucbComputation")
+	
+	U = (sd.I+1):(sd.I+sd.u(k,sd.I))
+
+	outputLevel > 0  && println("sd.I=$(sd.I) sd.u(k,sd.I)=$(sd.u(k,sd.I)) sd.s(k,sd.I)=$(sd.s(k,sd.I))")
+
+	for i in U
+		try
+				push!(sd.f,consume(problem.getNextSampleFunction))
+		catch
+			error("Not producing anymore! Probably out of datapoints.")
+		end
+	end
+
+	# S is composed of chunk indices
+	S = StatsBase.sample(1:sd.I,sd.s(k,sd.I);replace=false) 
+
+	# B is the current sum of old gradients (for S)
+	B = spzeros(sd.numVars,1)
+	for i in [S]
+		B +=sd.y[i]
+	end
+
+	# sumy is the new sum of gradients (for both U and S)
+	sumy = spzeros(sd.numVars,1)	
+
+	for i in [U]
+		thisSum = spzeros(sd.numVars,1)
+			(f,sampleG) = (sd.f[i])(x)
+			# MIGHT BE SLOW
+		thisSum+=sparse(sampleG)
+		push!(sd.y,thisSum)
+		sumy += thisSum
+	end
+
+	for i in [S]
+		thisSum = spzeros(sd.numVars,1)	
+			(f,sampleG) = (sd.f[i])(x)
+			# MIGHT BE SLOW
+			thisSum+=sparse(sampleG)
+		sd.y[i]=thisSum
+		sumy += thisSum
+	end
+
+	gnum += sd.chunkSize * (sd.s(k,sd.I)+sd.u(k,sd.I))
+	
+	if sd.sagFsagaT == false
+		g = (sd.A - B + sumy )/((sd.I+sd.u(k,sd.I)))
+	else
+		g = ((sd.s(k,sd.I) > 0  ? sd.s(k,sd.I)*((sd.beta(k)/sd.I)*sd.A) - sd.beta(k)*B : 0) + sumy )/((sd.s(k,sd.I)+sd.u(k,sd.I)))
+	end
+	
+	sd.I = sd.I + sd.u(k,sd.I)
+	
+	sd.A=sd.A-B+sumy
+	
+	(g, gnum)
+end
